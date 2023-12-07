a0b5cba0c91d82b73180d2667756619279a8a49c
Jawed <jawed.aquib874@gmail.com>
2023-12-07 16:46:56 +0530

    Fix Bugs

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const pianoKey = document.querySelectorAll(".piano-key .key")
 let audio = new Audio("Audio/a.ogg");  // By default a is playing
 
 const playmusic = (key)=>{    
-    audio.src = `Audio/${key}.ogg`;   // Passing in the src according the ley is pressed.
+    audio.src = `Audio/${key}.ogg`;   // Passing in the src according the key is pressed.
     audio.play();                     // Play the audio
 
     const clickedKey = document.querySelector(`[data-key="${key}"]`);
@@ -15,15 +15,16 @@ const playmusic = (key)=>{
 
 
 pianoKey.forEach((key)=>{
-    console.log(key)
-    // console.log(key.dataset.key)                // To find which button is pressed
    key.addEventListener("click", ()=> playmusic(key.dataset.key))            
 })
 
 const pressedkey = (e)=>{
+    console.log(e)
     playmusic(e.key);
 
-    // console.log(e)
 }
 
 document.addEventListener("keydown", pressedkey)
+
+
+
